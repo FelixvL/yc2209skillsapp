e8f307f374f277c3916680aa9d8ddc07fdf37bfd
FelixvL <digitaleservice@gmail.com>
2022-09-20 11:38:30 +0200

    eerste goede commit met application.properties

---
+++ src/main/java/nl/yc2209/skillapp/SkillappApplication.java
@@ -0,0 +1,13 @@
+package nl.yc2209.skillapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/yc2209/skillapp/SkillappApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.yc2209.skillapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
