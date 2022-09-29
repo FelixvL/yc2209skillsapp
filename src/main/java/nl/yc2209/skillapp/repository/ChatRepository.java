0ca061382438e337d6161fdd9cdb589273b04083
faydewaal <fay_de_waal@hotmail.com>
2022-09-29 13:05:15 +0200

    aanpassingen

--- src/main/java/nl/yc2209/skillapp/repository/ChatRepository.java
+++ src/main/java/nl/yc2209/skillapp/repository/ChatRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRepository extends JpaRepository { 
+public interface ChatRepository extends JpaRepository {
 
 }
