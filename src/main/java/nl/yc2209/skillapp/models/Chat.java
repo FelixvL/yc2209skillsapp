f62075f7d3abf22a8c10035a72b3853cf040a81d
faydewaal <fay_de_waal@hotmail.com>
2022-10-03 10:34:14 +0200

    foutje weggehaald

--- src/main/java/nl/yc2209/skillapp/models/Chat.java
+++ src/main/java/nl/yc2209/skillapp/models/Chat.java
@@ -1,9 +1,7 @@
 package nl.yc2209.skillapp.models;
 
-import org.aspectj.bridge.Message;
-
 import javax.persistence.*;
-import java.util.List;
+
 
 @Entity
 public class Chat {
@@ -11,10 +9,6 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    @OneToMany
-    //A list containing objects of type 'Message'. The list is called 'messages'
-    List<Message> messages; //using plural here for it a chat can have many messages
 
     public Chat(){} // you need this in order to post
 
@@ -25,9 +19,5 @@ public class Chat {
         this.id = id;
     }
 
-    public List<Message> getMessage(){return messages;}
 
-    public void setMessages(List <Message> messages){
-        this.messages = messages;
-    }
 }
